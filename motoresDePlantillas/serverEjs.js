865468bb8766dbdc8b0ef91e2d8b31fe1b4ee37c
federico-dev-web <federico.ls@hotmail.com>
2022-11-20 19:34:18 -0300

    Desafìo Motores de Plantillas

---
+++ motoresDePlantillas/serverEjs.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const Contenedor = require('./entregableManejoDeArchivos.js')
+const { Router } = express
+
+
+/// Creando contenedor 
+const productos = new Contenedor('productos')
+
+
+/////  Servidor
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const PORT = 8080
+
+routerApi = new Router()
+
+
+app.set('views', './viewsEjs')
+app.set('view engine', 'ejs')
+
+
+//formulario de carga
+app.get('/', (req, res) => {
+    res.render('main')
+})
+
+
+//devuelve todos los productos
+routerApi.get('/productos', async (req, res) => {
+    const data = await productos.getAll()
+    res.render('tabla', {data})
+})
+
+
+//recibe y agrega un producto, y lo devuelve con su id asignado
+routerApi.post('/productos', async (req, res) => {
+    console.log(req.body);
+    await productos.save(req.body)
+    res.redirect('/')
+})
+
+
+/////
+
+app.use(routerApi)
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+
+
